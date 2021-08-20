3a73c2d390afe31e50fbd2263ae08f3e019e1404
HumAiz Shahid <humaiz.work@gmail.com>
2021-08-20 23:35:39 +0500

    master

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -71,10 +71,8 @@ class ReportController extends Controller
                 'to' => $user_data->email,
                 'from' => env("MAIL_USERNAME"),
                 'url' => env("HOST_HTTP_REPORT"),
-                'subject' => 'Report',
                 'type' => $user_data->type,
                 'range' => $user_data->range,
-                'title' => 'Call Report',
                 "body"  => 'Hello'
             ];
     
