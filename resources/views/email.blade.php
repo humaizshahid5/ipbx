203dc31d352bf84649b28dc177a3f423c9d23321
HumAiz Shahid <humaiz.work@gmail.com>
2021-09-04 10:13:04 +0500

    master

--- resources/views/email.blade.php
+++ resources/views/email.blade.php
@@ -11,9 +11,9 @@ Your report filter:<br>
 @elseif($details['type'] == '3') Outgoing
 @elseif($details['type'] == '4') All 
 @endif <br>
-<b>Source</b>: $details['source']<br>
-<b>Destination</b>: $details['destination']<br>
-<b>Duration</b>: $details['duration']<br>
+<b>Source</b>: {{ $details['source'] }}<br>
+<b>Destination</b>: {{ $details['destination'] }}<br>
+<b>Duration</b>: {{ $details['duration'] }}<br>
 
 
 Call Report is ready to download. Please click here to download the report <a href="http://{{ $details['url'] }}/pdf/{{ $details['id'] }}/id">Download</a></h3>
