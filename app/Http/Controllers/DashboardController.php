31fde609c549c12f30cf65a1faa398e3d9e64c40
HumAiz Shahid <humaiz.work@gmail.com>
2021-09-06 23:35:57 +0500

    master

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -37,11 +37,11 @@ class DashboardController extends Controller
         $calls_today =   DB::table('cdr')->where('calltype', '=', '3')->Where('billsec', '>=', '1' )->where('calldate', 'like', date("Y-m-d")."%")->count();
         $users_count =   DB::table('users')->count();
         $calls =   DB::table('cdr as call')
-        ->leftJoin('phonebooks as s_name', 'call.source', '=', 's_name.number')
-        ->leftJoin('phonebooks as d_name', 'call.destination', '=', 'd_name.number')
+        ->leftJoin('phonebooks as s', 'call.source', '=', 's.number')
+        ->leftJoin('phonebooks as d', 'call.destination', '=', 'd.number')
         ->where('calltype', '=', '3')->Where('billsec', '>=', '1' )
         ->wherebetween('calldate', [$start_date,$end_date])->orderBY('calldate', 'DESC')
-        ->select('call.*', 'd_name.number as d_number', 'd_name.name as d_name','s_name.name as s_name','s_name.number as s_number')
+        ->select('call.*', 'd.number as d_number', 'd.name as d_name','s.name as s_name','s.number as s_number')
         ->get();
         $calls_total =   DB::table('cdr')->where('calltype', '=', '3')->Where('billsec', '>=', '1' )->whereDate('calldate', '>=', $start_date)->whereDate('calldate', '<=', $end_date)->orderBY('calldate', 'DESC')->get();
 
