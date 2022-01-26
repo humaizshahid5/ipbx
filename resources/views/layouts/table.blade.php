478d96f2b8208956fcb12d8bbf8d3f3aa35f1c02
HumAiz Shahid <humaiz.work@gmail.com>
2022-01-26 08:17:39 +0500

    master

--- resources/views/layouts/table.blade.php
+++ resources/views/layouts/table.blade.php
@@ -3,85 +3,77 @@
 
 @php
 
-
-                        
-                         
-                       
-
-    
-    $GLOBALS['c_rate'] = 0;
-    $GLOBALS['p_name'] = 0;
-    $GLOBALS['c_cost'] = 0;
-    $GLOBALS['loop_end'] = 0;
-    $GLOBALS['c_count'] = 0;
-    $GLOBALS['t_cost'] = 0;
-
-     
-                       
-
-    
+  $GLOBALS['c_rate'] = 0;
+  $GLOBALS['p_name'] = 0;
+  $GLOBALS['c_cost'] = 0;
+  $GLOBALS['loop_end'] = 0;
+  $GLOBALS['c_count'] = 0;
+  $GLOBALS['t_cost'] = 0;
   function add_some_extra($price_data,$call_data,&$call,&$rate)
   {
-                             $p_values = array();
-                            $c_values = array();
-                            $p_num = array();
-                            $status = 0;
-                            if(preg_match("/[a-z]/i", $call_data)){
-                             
-                            }
-                            if(strlen($price_data) == strlen($call_data) && $call->calltype == $rate->type){
-                            for($i = 0, $length = strlen($price_data); $i < $length; $i++) {
-                                if(is_numeric($price_data[$i])){
-                                        $p_values[] =$i." | ".$price_data[$i]."<br>";
-                                        $p_num[] = $i;
-                                }
-                              }
-                              for($i = 0, $length = strlen($call_data); $i < $length; $i++) {
-                                  $c_values[] = $i." | ".$call_data[$i]. "<br>";
-                              }
-                              $num = count($p_num);
-                              $m_count= 0;
-                              for($i = 0; $i < $num; $i++){
-                              $p_val = $p_num[$i];
-                              if($p_values[$i] == $c_values[$p_val]){
-                                $m_count = $m_count+1;
-                              $status = 1;
-                              }
-                              else{
-                              $status = 0;
-                              break;
-                              }
-                              }
-                            if($status == 1){
-                              if($m_count >  $GLOBALS['c_count'])
-                              {
-                                if($call->billsec <=	$rate->grace)
-                                {
-                                  $GLOBALS['c_rate'] = $rate->rate;
-                                  $GLOBALS['p_name'] = $rate->name;
-                                  $free = true;
-                                }
-                              else{
-                                if($rate->rate == 0){
-                                  $GLOBALS['c_cost'] = 0;
-                                  $GLOBALS['p_name'] = $rate->name;
-                                }
-                                else{
-                                  $GLOBALS['c_count'] = $m_count;
-                                  $GLOBALS['c_rate'] = $rate->rate;
-                                  $GLOBALS['p_name'] = $rate->name;
-                                 
-                                  $GLOBALS['c_cost'] =round ( $rate->rate / 60 * ( $call->billsec <= $rate->minimal ? $rate->minimal : ceil ( $call->billsec / $rate->fraction) * $rate->fraction), 2);
-                                  $t_cost =  $GLOBALS['t_cost']+$GLOBALS['c_cost'];
-                                }
-                              }
-                              
-                            }
-                          }
-
-                        }
-      
-     
+    $p_values = array();
+    $c_values = array();
+    $p_num = array();
+    $status = 0;
+    if(preg_match("/[a-z]/i", $call_data)){
+      return;
+    }
+    if(strlen($price_data) == strlen($call_data) && $call->calltype == $rate->type)
+    {
+      for($i = 0, $length = strlen($price_data); $i < $length; $i++)
+      {
+          if(is_numeric($price_data[$i])){
+                  $p_values[] =$i." | ".$price_data[$i]."<br>";
+                  $p_num[] = $i;
+          }
+      }
+      for($i = 0, $length = strlen($call_data); $i < $length; $i++)
+      {
+        $c_values[] = $i." | ".$call_data[$i]. "<br>";
+      }
+      $num = count($p_num);
+      $m_count= 0;
+      for($i = 0; $i < $num; $i++)
+      {
+        $p_val = $p_num[$i];
+        if($p_values[$i] == $c_values[$p_val])
+        {
+          $m_count = $m_count+1;
+          $status = 1;
+        }
+        else{
+          $status = 0;
+          break;
+        }
+        }
+      if($status == 1)
+      {
+        if($m_count >  $GLOBALS['c_count'])
+        {
+          if($call->billsec <=	$rate->grace)
+          {
+            $GLOBALS['c_rate'] = $rate->rate;
+            $GLOBALS['p_name'] = $rate->name;
+            $GLOBALS['free'] = true;
+          }
+          else
+          {
+            if($rate->rate == 0)
+            {
+              $GLOBALS['c_cost'] = 0;
+              $GLOBALS['p_name'] = $rate->name;
+            }
+            else{
+              $GLOBALS['c_count'] = $m_count;
+              $GLOBALS['c_rate'] = $rate->rate;
+              $GLOBALS['p_name'] = $rate->name;   
+              $GLOBALS['c_cost'] =round ( $rate->rate / 60 * ( $call->billsec <= $rate->minimal ? $rate->minimal : ceil ( $call->billsec / $rate->fraction) * $rate->fraction), 2);
+              $t_cost =  $GLOBALS['t_cost']+$GLOBALS['c_cost'];
+            }
+          }
+        }
+      } 
+    }
   }
 
 @endphp
@@ -111,27 +103,21 @@
                     @php 
                       $GLOBALS['c_count'] = 0;               
                       $GLOBALS['c_rate'] = 0;
-                      $GLOBALS['p_name'] = 0;
+                      $GLOBALS['p_name'] = "";
                       $GLOBALS['c_cost'] = 0;
                       $GLOBALS['loop_end'] = 0;
-                      
-                      $free = false;
+                      $GLOBALS['free'] = false;
                       $t_duration = $t_duration+$call->billsec;
                     @endphp
                   <tr>
                     <td style="display:none;">@php $number_count=$number_count+1; @endphp</td>
                     <td>{{ date('M j,Y H:i', strtotime('-1 hours', strtotime($call->calldate))) }} </td>
-                   
                     <td>
-                      
                       @if($call->source == $call->s_number)
                       <a href="#{{ $call->cdr_id }}" data-toggle="tooltip_source{{ $call->cdr_id }}" title="{{ $call->source }}">
                       {{ $call->s_name }}
                       </a>
-                      @else
-                      {{ $call->source }}
-
-                      @endif
+                      @else{{ $call->source }}@endif
                     </td>
                     <td>
                       @if($call->destination == $call->d_number)
@@ -204,8 +190,6 @@
                               add_some_extra($price_data,$call_data,$call,$rate);
                             }
                           }
-                                                          
-                            
                           @endphp
                       @endforeach
                     @php
@@ -220,15 +204,11 @@
                     else{
                       echo $GLOBALS['p_name'];
                     }
-                    
-                         
-                      
-                    
                    @endphp
                     </td>
                     <td>@php echo number_format(floatval($GLOBALS['c_rate']), 2, ',', '');  @endphp</td>
                     <td>@php  
-                      if($free == true){
+                      if( $GLOBALS['free'] == true){
                         echo "Free";
                       }
                       else{
